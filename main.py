717b1c1da33cb8e14d527ac6bb0d11e6bdf6b77c
bghj25 <48888287+bghj25@users.noreply.github.com>
2023-11-13 02:32:59 +0300

    Собирается статистика по полу и взорасту, вывод в json

--- main.py
+++ main.py
@@ -1,47 +1,94 @@
 import requests
 import json
+import datetime
+import data
+import matplotlib.pyplot as plt
 
 
-token = "c1598721c1598721c159872141c12b806acc159c15987219f9a265511f76681353f3a40"
-gid = "rolecon"
+
+
+
 fld = "sex, bdate"
 vers = 5.154
-payload = {'access_token': token,
-           'group_id': gid,
-           'fields': fld,
-           'offset': 8000,
-           'v': vers
-          }
+publics_data = {}
 
-members = []
-end_of_members_flag = False
-offset = 1000
-while not end_of_members_flag:
-    r = requests.get("https://api.vk.com/method/groups.getMembers", params=payload).text
-    data_dict = json.loads(r)
-    members += data_dict["response"]["items"]
-    if data_dict["response"]["next_from"] == "":
-        end_of_members_flag = True
-    else:
-        payload["offset"] = offset
-        offset += 1000
-        #time.sleep(5)
+for gid in data.gids:
+    payload = {'access_token': data.token,
+               'group_id': gid,
+               'fields': fld,
+               'offset': 0,
+               'v': vers
+              }
 
-print(len(members))
-male = 0
-female = 0
-for member in members:
-    if member["sex"] == 1:
-        female += 1
-    elif member["sex"] == 2:
-        male += 1
-print(female)
-print(male)
-print(male + female)
+    members = []
+    end_of_members_flag = False
+    offset = 1000
+    while not end_of_members_flag:
+        r = requests.get("https://api.vk.com/method/groups.getMembers", params=payload).text
+        data_dict = json.loads(r)
+        members += data_dict["response"]["items"]
+        if data_dict["response"]["next_from"] == "":
+            end_of_members_flag = True
+        else:
+            payload["offset"] = offset
+            offset += 1000
+            #time.sleep(5)
+
+    print(len(members))
+    male = 0
+    female = 0
+    today = datetime.date.today()
+
+    ages = {}
+    ful_date = 0
+    half_date = 0
+    no_date = 0
+    for member in members:
+        if member["sex"] == 1:
+            female += 1
+        elif member["sex"] == 2:
+            male += 1
+
+        try:
+            bd = member["bdate"].split(".")
+        except KeyError:
+            no_date += 1
+            pass
+
+        if len(bd) == 3:
+            birthday = datetime.date(int(bd[2]), int(bd[1]), int(bd[1]))
+            ful_date += 1
+            try:
+                ages[(today - birthday).days // 365] += 1
+            except KeyError:
+                ages[(today - birthday).days // 365] = 1
+        else:
+            half_date += 1
+            # if (today - birthday).days // 365 == 122:
+            #     print("vk.com/id{}".format(member["id"]))
+            #print((today - birthday).days // 365)
+
+    sorted_ages = dict(sorted(ages.items()))
 
 
 
-# payload['start_from'] = data_dict["response"]["next_from"]
-# r = requests.get("https://api.vk.com/method/groups.getMembers", params=payload).text
-# data_dict = json.loads(r)
-# print(data_dict["response"]["next_from"])
+    #print(ages)
+
+    # plt.bar(range(len(sorted_ages)), list(sorted_ages.values()), align='center')
+    # plt.xticks(range(len(sorted_ages)), list(sorted_ages.keys()), rotation=90)
+    # plt.show()
+
+    print(sorted_ages)
+    publics_data[gid] = {"Всего подписчиков: ": len(members),
+                         "Из них мужчины: ": male,
+                         "Из них женщины: ": female,
+                         "Возрастная характеристика: ": {"Не указана дата рождения: ": no_date,
+                                                         "Частично указана дата рождения:": half_date,
+                                                         "Полностью дата рождения:": ful_date,
+                                                         "Статистика по возрастам(на основании {}"
+                                                         " человек с полной датой".format(ful_date): ages}
+                         }
+
+
+with open("sample.json", "w") as outfile:
+    json.dump(publics_data, outfile)
