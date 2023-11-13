2f060c9fd4246698d8e321375e4527d0396386f1
bghj25 <48888287+bghj25@users.noreply.github.com>
2023-11-13 03:19:26 +0300

    Пофиксил баги

--- main.py
+++ main.py
@@ -5,20 +5,18 @@ import data
 import matplotlib.pyplot as plt
 
 
-
-
-
 fld = "sex, bdate"
 vers = 5.154
-publics_data = {}
+publics_data = {"Актуально на ": str(datetime.datetime.today().strftime("%d/%m/%Y %H:%M:%S"))}
 
-for gid in data.gids:
-    payload = {'access_token': data.token,
+for gid in data.gids:  # Указать в data.py список пабликов
+    print(gid)
+    payload = {'access_token': data.token,  # Указать в data.py токен
                'group_id': gid,
                'fields': fld,
                'offset': 0,
                'v': vers
-              }
+               }
 
     members = []
     end_of_members_flag = False
@@ -32,9 +30,9 @@ for gid in data.gids:
         else:
             payload["offset"] = offset
             offset += 1000
-            #time.sleep(5)
+            # time.sleep(5)
 
-    print(len(members))
+
     male = 0
     female = 0
     today = datetime.date.today()
@@ -51,22 +49,21 @@ for gid in data.gids:
 
         try:
             bd = member["bdate"].split(".")
+            if len(bd) == 3:
+                birthday = datetime.date(int(bd[2]), int(bd[1]), int(bd[1]))
+                ful_date += 1
+                try:
+                    ages[(today - birthday).days // 365] += 1
+                except KeyError:
+                    ages[(today - birthday).days // 365] = 1
+            else:
+                half_date += 1
+                # if (today - birthday).days // 365 == 122:
+                #     print("vk.com/id{}".format(member["id"]))
+                # print((today - birthday).days // 365)
         except KeyError:
             no_date += 1
             pass
-
-        if len(bd) == 3:
-            birthday = datetime.date(int(bd[2]), int(bd[1]), int(bd[1]))
-            ful_date += 1
-            try:
-                ages[(today - birthday).days // 365] += 1
-            except KeyError:
-                ages[(today - birthday).days // 365] = 1
-        else:
-            half_date += 1
-            # if (today - birthday).days // 365 == 122:
-            #     print("vk.com/id{}".format(member["id"]))
-            #print((today - birthday).days // 365)
 
     sorted_ages = dict(sorted(ages.items()))
 
@@ -78,17 +75,18 @@ for gid in data.gids:
     # plt.xticks(range(len(sorted_ages)), list(sorted_ages.keys()), rotation=90)
     # plt.show()
 
-    print(sorted_ages)
+    # print(sorted_ages)
     publics_data[gid] = {"Всего подписчиков: ": len(members),
                          "Из них мужчины: ": male,
                          "Из них женщины: ": female,
+                         "Неопределившихся: ": len(members) - male - female,
                          "Возрастная характеристика: ": {"Не указана дата рождения: ": no_date,
                                                          "Частично указана дата рождения:": half_date,
                                                          "Полностью дата рождения:": ful_date,
                                                          "Статистика по возрастам(на основании {}"
-                                                         " человек с полной датой".format(ful_date): ages}
+                                                         " человек с полной датой".format(ful_date): sorted_ages}
                          }
 
 
 with open("sample.json", "w") as outfile:
-    json.dump(publics_data, outfile)
+    json.dump(publics_data, outfile, ensure_ascii=False, indent=4)
